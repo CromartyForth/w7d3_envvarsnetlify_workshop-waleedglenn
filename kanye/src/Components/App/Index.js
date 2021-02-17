c354090cb40f41ad80024e4a1b65e4fd8e34b07c
alrwaishan <alrwaishanw@gmail.com>
2021-02-17 16:46:54 +0000

    removed git from bottom

--- kanye/src/Components/App/Index.js
+++ kanye/src/Components/App/Index.js
@@ -28,7 +28,7 @@ function handleClick() {
         <h1>{quote}</h1>
         <img src='https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed00f17d4a99d0006d2e738%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D154%26cropX2%3D4820%26cropY1%3D651%26cropY2%3D5314' className="App-logo" alt="kayne" />
         
-        <button onClick={handleClick} >Get Quotegit</button>
+        <button onClick={handleClick} >Get Quote</button>
       </header>
     </div>
   );
