7757f555ed216e15d1c3055ea5d7bddb0f3f056a
alrwaishan <alrwaishanw@gmail.com>
2021-02-17 16:10:18 +0000

    Completed the rest API App

--- kanye/src/Components/App/Index.js
+++ kanye/src/Components/App/Index.js
@@ -25,18 +25,10 @@ function handleClick() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{quote}</h1>
         <img src='https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed00f17d4a99d0006d2e738%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D154%26cropX2%3D4820%26cropY1%3D651%26cropY2%3D5314' className="App-logo" alt="kayne" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <button onClick={handleClick} >Get Quotegit</button>
       </header>
     </div>
   );
